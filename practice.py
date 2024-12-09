1cf2105a8d407c7a21208084db120197d9a7f02a
MaratSav <marat1620@gmail.com>
2024-12-09 12:57:29 +0300

    сортировка пузырьковая, выбором, вставкой

--- practice.py
+++ practice.py
@@ -1,29 +1,14 @@
-# Пузырьковая сортировка (сортировка обменами)
-
-nums = [5, 6, 2, 1, 3, 4]
-
-def bubble_sort(ls):
-    swapped = True
-    while swapped:
-        swapped = False
-        for i in range(len(ls) - 1):
-            if ls[i] > ls[i+1]:
-                ls[i], ls[i+1] = ls[i+1], ls[i]  # Меняет местами значения
-                swapped = True
-
-bubble_sort(nums)
-print(nums)
+from mod4.practice_1_2 import *
 
 
-# Сортировка выбором
+data_1 = list(map(int, input("Введите число через пробел ").split()))
+data_2 = list(map(int, input("Введите число через пробел ").split()))
+data_3 = list(map(int, input("Введите число через пробел ").split()))
 
-def selection_sort(ls):
-    for i in range(len(ls)):
-        lowest = i
-        for j in range(i + 1, len(ls)):
-            if ls[j] < ls[lowest]:
-                lowest = j
-        ls[i], ls[lowest] = ls[lowest], ls[i]
+bubble_sort(data_1)
+selection_sort(data_2)
+insertion_sort(data_3)
 
-selection_sort(nums)
-print(nums)
+print("Пузырьковая сортировка:", data_1)
+print("Сортировка выбором:", data_2)
+print("Сортировка вставкой:", data_3)
